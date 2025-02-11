9c62106da2b5b15d7d952821beda4f34a29facf7
Luis George <luis.fernando.george.u@gmail.com>
2025-02-10 18:59:45 -0600

    creacion de la estructura del proyecto y clases model

---
+++ src/main/java/com/ecommerproyecto/EcommerproyectoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerproyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerproyecto/EcommerproyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerproyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
