263c29a8753083289bf20b508b22e0ae29af821e
Luis George <luis.fernando.george.u@gmail.com>
2025-02-14 01:20:25 -0600

    obtener la cantidad y el id para anadir en el carrito

--- src/main/java/com/ecommerproyecto/controller/HomeController.java
+++ src/main/java/com/ecommerproyecto/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ecommerproyecto.controller;
 
+import com.ecommerproyecto.model.DetalleOrden;
+import com.ecommerproyecto.model.Orden;
 import com.ecommerproyecto.model.Producto;
 import com.ecommerproyecto.repository.ProductoRepository;
 import com.ecommerproyecto.service.ProductoService;
@@ -8,11 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -23,6 +24,12 @@ public class HomeController {
 
     @Autowired
     private ProductoService productoService;
+
+    //para almacenar los detalles de la orden
+    private List<DetalleOrden> detalleOrdens = new ArrayList<DetalleOrden>();
+
+    //almacena datos de la orden
+    Orden orden = new Orden();
 
 
     @GetMapping("")
@@ -45,7 +52,18 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart() {
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal=0;
+
+        Optional<Producto> optionalProducto = productoService.get(id);
+        log.info("El producto ananido : {}", optionalProducto.get());
+        log.info("Cantidad : {}", cantidad);
+
+
+
+
         return "usuario/carrito";
     }
 
