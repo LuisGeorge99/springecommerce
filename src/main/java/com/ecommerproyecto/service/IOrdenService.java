eedfc2eba776dbd4c1cf24533c644ba4cd11604c
Luis George <luis.fernando.george.u@gmail.com>
2025-02-17 18:42:37 -0600

    metodo para generar el numero de la orden

--- src/main/java/com/ecommerproyecto/service/IOrdenService.java
+++ src/main/java/com/ecommerproyecto/service/IOrdenService.java
@@ -2,6 +2,11 @@ package com.ecommerproyecto.service;
 
 import com.ecommerproyecto.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+
+    List<Orden> findAll();
+
     Orden save (Orden orden);
 }
--- src/main/java/com/ecommerproyecto/service/OrdenServiceImpl.java
+++ src/main/java/com/ecommerproyecto/service/OrdenServiceImpl.java
@@ -5,6 +5,9 @@ import com.ecommerproyecto.repository.IOrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServiceImpl implements IOrdenService{
 
@@ -12,7 +15,42 @@ public class OrdenServiceImpl implements IOrdenService{
     private IOrdenRepository ordenRepository;
 
     @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
+    }
+
+    public String generarNumeroOrden() {
+        int numero=0;
+        String numeroConCatenado ="";
+
+        List<Orden> ordenes = findAll();
+        List<Integer> numeros = new ArrayList<Integer>();
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if(ordenes.isEmpty()) {
+            numero=1;
+
+        }else {
+            numero= numeros.stream().max(Integer::compare).get();
+            numero++;
+        }
+
+        if(numero<10) {
+            numeroConCatenado="000000000"+String.valueOf(numero);
+        }else if (numero<100) {
+            numeroConCatenado="00000000"+String.valueOf(numero);
+        }else if (numero<1000) {
+            numeroConCatenado="0000000"+String.valueOf(numero);
+        }else if (numero<10000) {
+            numeroConCatenado="000000"+String.valueOf(numero);
+        }
+
+
+        return numeroConCatenado;
     }
 }
